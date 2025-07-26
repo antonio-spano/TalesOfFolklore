f6d9327d4cb269d3e3b092c94ae61ba68a857530
TonyDev <antoniospano05@outlook.it>
2025-07-26 23:28:07 +0200

    Added citadel

--- src/main/java/net/spanoprime/talesoffolklore/worldgen/injector/BiomeInjector.java
+++ src/main/java/net/spanoprime/talesoffolklore/worldgen/injector/BiomeInjector.java
@@ -1,11 +1,14 @@
 package net.spanoprime.talesoffolklore.worldgen.injector;
 
+import com.github.alexthe666.citadel.server.world.ExpandedBiomes;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,6 +25,7 @@ public class BiomeInjector {
     public static void onServerAboutToStart(ServerAboutToStartEvent event) {
         Registry<Biome> biomeRegistry = event.getServer().registryAccess().registryOrThrow(Registries.BIOME);
         APPALACHIAN_FOREST_HOLDER = biomeRegistry.getHolderOrThrow(ModBiomes.APPALACHIAN_FOREST);
+        ExpandedBiomes.addExpandedBiome(ModBiomes.APPALACHIAN_FOREST, LevelStem.OVERWORLD);
 
         System.out.println("[TALES OF FOLKLORE] BIOME INJECTOR ARMED. Holder captured: " + APPALACHIAN_FOREST_HOLDER.unwrapKey());
     }
